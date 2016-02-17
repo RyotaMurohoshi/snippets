4bcf59a63f4cab5b5e46d1bbb6b2db4aca16a956
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-02-18 03:27:38 +0900

    created diamond operator example

---
+++ java/diamond_operator_example/src/com/muhron/diamond_operator_example/Main.java
@@ -0,0 +1,15 @@
+package com.muhron.diamond_operator_example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        System.out.println(list.size());
+    }
+}
+
+
+
