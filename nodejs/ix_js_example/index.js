8a3070039fb068e5096ce81c9188d3194537a1a9
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-02-23 23:34:07 +0900

    added

--- nodejs/ix_js_example/index.js
+++ nodejs/ix_js_example/index.js
@@ -50,5 +50,7 @@ console.log(lookup.get(0).toArray());
 console.log(lookup.get(1).toArray());
 
 
+console.log(Ix.Enumerable.repeat(0, 10).toArray());
+
 console.log(source.takeLast(3).toArray());
 console.log(source.skipLast(3).toArray());
