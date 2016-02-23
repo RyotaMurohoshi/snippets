5fd2c19f2626f461e309cd0bb23a0bb058bf775f
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-02-24 01:44:55 +0900

    converted to kotlin

---
+++ kotlin/anko_example/app/src/main/kotlin/com/muhron/anko_example/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.muhron.anko_example
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+    }
+}
