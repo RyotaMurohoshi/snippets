e456580ea2f8130046198189df3358bf549872b6
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-02 23:56:41 +0900

    init project

---
+++ groovy/hello_ast_transform/com/muhron/hello_ast_transform/Person.groovy
@@ -0,0 +1,9 @@
+package com.muhron.hello_ast_transform
+
+import groovy.transform.ToString
+
+@ToString(includePackage = false)
+class Person{
+    String name;
+    int age;
+}
---
+++ groovy/hello_ast_transform/example.groovy
@@ -0,0 +1,3 @@
+import com.muhron.hello_ast_transform.Person;
+
+println(new Person(name:"Ryota", age:27))
