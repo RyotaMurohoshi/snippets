757e1d922c24267193891658c03b319f2d284d86
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-04-26 23:55:44 +0900

    added script example indexed

--- groovy/index_mathod/script.groovy
+++ groovy/index_mathod/script.groovy
@@ -3,3 +3,8 @@ assert [["A", 0]] == ["A"].withIndex()
 assert [["A", 0], ["B", 1]] == ["A", "B"].withIndex()
 assert [["A", 0], ["B", 1], ["C", 2]] == ["A", "B", "C"].withIndex()
 assert ["A", "C", "E"] == ["A", "B", "C", "D", "E", "F"].withIndex().findAll{ e, i -> i % 2 == 0 }.collect { e , i ->  e}
+
+assert [:] == [].indexed()
+assert [0 : "A"] == ["A"].indexed()
+assert [0 : "A", 1 : "B"] == ["A", "B"].indexed()
+assert [0 : "A", 1 : "B", 2 : "C"] == ["A", "B", "C"].indexed()
