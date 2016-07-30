5af8077a2a94f10fddaef66afaa65deddf3e5e04
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-07-29 22:31:37 -0700

    example JsonBuilder

--- groovy/snipetts/json_playground.groovy
+++ groovy/snipetts/json_playground.groovy
@@ -8,3 +8,10 @@ def jsonString = """{
 def json = new JsonSlurper().parseText(jsonString)
 assert json.name == "RyotaMurohoshi"
 assert json.languages == ["C#", "Java", "Kotlin", "Groovy"]
+
+def map =  [
+  name:"RyotaMurohoshi",
+  "languages": ["C#", "Java", "Kotlin", "Groovy"]
+]
+
+assert new JsonBuilder(map).toString() == '{"name":"RyotaMurohoshi","languages":["C#","Java","Kotlin","Groovy"]}'
