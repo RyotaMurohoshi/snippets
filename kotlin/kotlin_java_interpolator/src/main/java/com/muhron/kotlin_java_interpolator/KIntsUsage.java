ba5e8a0a771fb1a98758be105262357dd0a9cabc
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-12-12 19:45:58 +0900

    added java kotlin primitives

---
+++ kotlin/kotlin_java_interpolator/src/main/java/com/muhron/kotlin_java_interpolator/KIntsUsage.java
@@ -0,0 +1,9 @@
+package com.muhron.kotlin_java_interpolator;
+
+public class KIntsUsage {
+    public static void main(String[] args) {
+        int num0 = KInts.getInt();
+
+        Integer num1 = KInts.getNullableInt();
+    }
+}
