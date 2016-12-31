c5add071f95505c2300ecfd3d1ce9356eb17bb24
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-12-31 23:06:20 +0900

    added fs unity

---
+++ fsharp/fs_unity/unity/Assets/Main.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using FsUnity;
+
+public class Main : MonoBehaviour {
+	void Start () {
+        Point point = new Point(0, 0);
+        Debug.Log(point.X + ", " + point.Y);
+	}
+}
